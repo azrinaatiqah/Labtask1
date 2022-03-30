fa9c01a5fe855c5cad4479da9f8f1e835aa9f314
Acer <Acer@IQAA>
2022-03-31 01:56:02 +0800

    Add project files.

---
+++ Form1.Designer.vb
@@ -0,0 +1,150 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.BtnSum = New System.Windows.Forms.Button()
+        Me.TxtNum1 = New System.Windows.Forms.TextBox()
+        Me.TxtNum2 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.TxtResult = New System.Windows.Forms.TextBox()
+        Me.BtnClose = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'BtnSum
+        '
+        Me.BtnSum.BackColor = System.Drawing.Color.MediumOrchid
+        Me.BtnSum.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnSum.Location = New System.Drawing.Point(249, 308)
+        Me.BtnSum.Name = "BtnSum"
+        Me.BtnSum.Size = New System.Drawing.Size(75, 23)
+        Me.BtnSum.TabIndex = 0
+        Me.BtnSum.Text = "SUM"
+        Me.BtnSum.UseVisualStyleBackColor = False
+        '
+        'TxtNum1
+        '
+        Me.TxtNum1.BackColor = System.Drawing.Color.White
+        Me.TxtNum1.Location = New System.Drawing.Point(249, 152)
+        Me.TxtNum1.Name = "TxtNum1"
+        Me.TxtNum1.Size = New System.Drawing.Size(197, 23)
+        Me.TxtNum1.TabIndex = 1
+        '
+        'TxtNum2
+        '
+        Me.TxtNum2.BackColor = System.Drawing.Color.White
+        Me.TxtNum2.Location = New System.Drawing.Point(249, 198)
+        Me.TxtNum2.Name = "TxtNum2"
+        Me.TxtNum2.Size = New System.Drawing.Size(197, 23)
+        Me.TxtNum2.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Comic Sans MS", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.Label1.Location = New System.Drawing.Point(246, 66)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(200, 30)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "SUM OF NUMBER"
+        '
+        'TxtResult
+        '
+        Me.TxtResult.BackColor = System.Drawing.Color.White
+        Me.TxtResult.Location = New System.Drawing.Point(249, 250)
+        Me.TxtResult.Name = "TxtResult"
+        Me.TxtResult.Size = New System.Drawing.Size(197, 23)
+        Me.TxtResult.TabIndex = 4
+        '
+        'BtnClose
+        '
+        Me.BtnClose.BackColor = System.Drawing.Color.MediumOrchid
+        Me.BtnClose.Font = New System.Drawing.Font("Comic Sans MS", 9.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point)
+        Me.BtnClose.Location = New System.Drawing.Point(371, 308)
+        Me.BtnClose.Name = "BtnClose"
+        Me.BtnClose.Size = New System.Drawing.Size(75, 23)
+        Me.BtnClose.TabIndex = 5
+        Me.BtnClose.Text = "CLOSE"
+        Me.BtnClose.UseVisualStyleBackColor = False
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(127, 152)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(92, 15)
+        Me.Label2.TabIndex = 6
+        Me.Label2.Text = "FIRST NUMBER: "
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(127, 198)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(110, 15)
+        Me.Label3.TabIndex = 7
+        Me.Label3.Text = "SECOND NUMBER: "
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(127, 250)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(51, 15)
+        Me.Label4.TabIndex = 8
+        Me.Label4.Text = "RESULT: "
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.ClientSize = New System.Drawing.Size(713, 392)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.BtnClose)
+        Me.Controls.Add(Me.TxtResult)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TxtNum2)
+        Me.Controls.Add(Me.TxtNum1)
+        Me.Controls.Add(Me.BtnSum)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents BtnSum As Button
+    Friend WithEvents TxtNum1 As TextBox
+    Friend WithEvents TxtNum2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents TxtResult As TextBox
+    Friend WithEvents BtnClose As Button
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+End Class
---
+++ Form1.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form1
+
+    Private Sub BtnSum_Click(sender As Object, e As EventArgs) Handles BtnSum.Click
+
+        TxtResult.Text = (Convert.ToInt32(TxtNum1.Text) + Convert.ToInt32(TxtNum2.Text)).ToString()
+
+    End Sub
+
+    Private Sub BtnClose_Click(sender As Object, e As EventArgs) Handles BtnClose.Click
+        Close()
+    End Sub
+End Class
